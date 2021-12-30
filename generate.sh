71438cd319bbe9720e465fab0554b85882a3c9e1
Lucas Thiago Batista <lucas.batista@santander.com.br>
2021-12-30 11:56:02 -0300

    add server + proto

---
+++ generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+protoc squareroot/squarepb/square.proto --go_out=plugins=grpc:.
---
+++ squareroot/square_server/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"net"
+
+	"github.com/ltbatista/squareroot/squareroot/squarepb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/internal/status"
+)
+
+type server struct{}
+
+func (*server) SquareRoot(ctx context.Context, req *squarepb.SquareRootRequest) (*squarepb.SquareRootResponse, error) {
+	fmt.Printf("Squareroot function was invoked with %v\n", req)
+	number := req.GetNumber()
+
+	if number < 0 {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("Received a negative number: %v", number),
+		)
+	}
+	return &squarepb.SquareRootResponse{
+		NumberRoot: math.Sqrt(float64(number)),
+	}, nil
+}
+
+func main() {
+	fmt.Println("Hello, i'm the square root server...")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	squarepb.RegisterSquareRootServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
